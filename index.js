1fa434ecf7b38543cd7afece61904841cce89ea3
Virej Dasani <virej@Virejs-MacBook-Air.local>
2021-09-03 20:50:33 +0530

    Add resume functionality and fix bugs

--- index.js
+++ index.js
@@ -1,29 +1,42 @@
+// This is so that if speech is still playing from previous session, it stops on page load
+speechSynthesis.cancel();
+
 var isSpeaking = false;
+
+// Initialize the speech synthesis
+var speech = new SpeechSynthesisUtterance();
+speech.rate = 1;
+speech.pitch = 1;
+speech.volume = 1;
+speech.voice = speechSynthesis.getVoices()[0];
 
 function speakInputText() {
   isSpeaking = true;
 
-  // Initialize the speech
-  let speech = new SpeechSynthesisUtterance();
-  speech.rate = 1;
-  speech.pitch = 1;
-  speech.volume = 1;
-  speech.voice = speechSynthesis.getVoices()[0];
   speech.text = document.getElementById("textInput").value;
   speechSynthesis.speak(speech);
 }
 
 function pauseSpeech() {
   if (isSpeaking) {
-    speechSynthesis.pause();
     isSpeaking = false;
+    speechSynthesis.pause();
+    document.getElementById(
+      "pauseButton"
+    ).innerHTML = `Resume<img class="icon-link-small"
+    src="./assets/icons/resumeIcon.svg">`;
   } else {
-    speechSynthesis.resume();
     isSpeaking = true;
+    speechSynthesis.resume();
+    document.getElementById(
+      "pauseButton"
+    ).innerHTML = `Pause<img class="icon-link-small"
+    src="./assets/icons/pauseIcon.svg">`;
   }
 }
 
 function stopSpeech() {
+  isSpeaking = false;
   speechSynthesis.cancel();
 }
 
