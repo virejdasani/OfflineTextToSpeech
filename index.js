3de92fd57c0e66affc38f8324d7359f3330f1a54
Virej Dasani <virej@Virejs-MacBook-Air.local>
2021-09-05 00:09:43 +0530

    Add voice options and speed options

--- index.js
+++ index.js
@@ -40,6 +40,56 @@ function stopSpeech() {
   speechSynthesis.cancel();
 }
 
-// TODO
-// speech rate
-// speech voice
+function changeVoice(voice) {
+  if (voice == "voice1") {
+    // console.log((speech.voice = speechSynthesis.getVoices()[8]));
+    speech.voice = speechSynthesis.getVoices()[8];
+  } else if (voice == "voice2") {
+    // console.log((speech.voice = speechSynthesis.getVoices()[0]));
+    speech.voice = speechSynthesis.getVoices()[0];
+  } else if (voice == "voice3") {
+    // console.log((speech.voice = speechSynthesis.getVoices()[1]));
+    speech.voice = speechSynthesis.getVoices()[1];
+  } else if (voice == "voice4") {
+    // console.log((speech.voice = speechSynthesis.getVoices()[11]));
+    speech.voice = speechSynthesis.getVoices()[11];
+  } else if (voice == "voice5") {
+    // console.log((speech.voice = speechSynthesis.getVoices()[12]));
+    speech.voice = speechSynthesis.getVoices()[12];
+  } else if (voice == "voice6") {
+    // console.log((speech.voice = speechSynthesis.getVoices()[18]));
+    speech.voice = speechSynthesis.getVoices()[18];
+  } else if (voice == "voice7") {
+    // console.log((speech.voice = speechSynthesis.getVoices()[33]));
+    speech.voice = speechSynthesis.getVoices()[33];
+  } else if (voice == "voice8") {
+    // console.log((speech.voice = speechSynthesis.getVoices()[37]));
+    speech.voice = speechSynthesis.getVoices()[37];
+  } else if (voice == "voice9") {
+    // console.log((speech.voice = speechSynthesis.getVoices()[41]));
+    speech.voice = speechSynthesis.getVoices()[41];
+  }
+
+  // for (let i = 0; i < 100; i++) {
+  //   console.log((speech.voice = speechSynthesis.getVoices()[i]));
+  // }
+}
+
+function changeVoiceSpeed(voiceSpeed) {
+  // For some reason, speed below 0.5 doesn't work
+  if (voiceSpeed == "speed2") {
+    speech.rate = 2;
+  } else if (voiceSpeed == "speed1.75") {
+    speech.rate = 1.75;
+  } else if (voiceSpeed == "speed1.5") {
+    speech.rate = 1.5;
+  } else if (voiceSpeed == "speed1.25") {
+    speech.rate = 1.25;
+  } else if (voiceSpeed == "speed1") {
+    speech.rate = 1;
+  } else if (voiceSpeed == "speed0.75") {
+    speech.rate = 0.75;
+  } else if (voiceSpeed == "speed0.5") {
+    speech.rate = 0.5;
+  }
+}
